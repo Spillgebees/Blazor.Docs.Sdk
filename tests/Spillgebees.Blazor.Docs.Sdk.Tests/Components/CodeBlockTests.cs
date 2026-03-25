6a23818ae8d69a31d8d083cb47597922eb929909
Jason Rebelo Neves <igotinfected@gmail.com>
2026-03-25 13:53:07 +0100

    :bug: language highlighting for file types other than `razor`

--- tests/Spillgebees.Blazor.Docs.Sdk.Tests/Components/CodeBlockTests.cs
+++ tests/Spillgebees.Blazor.Docs.Sdk.Tests/Components/CodeBlockTests.cs
@@ -94,4 +94,109 @@ public class CodeBlockTests
         var copy = cut.Find(".code-block-copy");
         copy.Should().NotBeNull();
     }
+
+    [Test]
+    public void Should_infer_csharp_language_for_code_behind_file()
+    {
+        // arrange
+        using var ctx = new BunitContext();
+        ctx.JSInterop.SetupVoid("Spillgebees.DocsSdk.copyToClipboard", _ => true);
+        ctx.JSInterop.SetupVoid("Spillgebees.DocsSdk.highlightElement", _ => true);
+        var files = new Dictionary<string, string> { ["Counter.razor.cs"] = "public partial class Counter {}" };
+
+        // act
+        var cut = ctx.Render<CodeBlock>(parameters => parameters
+            .Add(p => p.Files, files)
+            .Add(p => p.Language, "razor"));
+
+        // assert
+        var code = cut.Find("pre code");
+        code.ClassList.Should().Contain("language-csharp");
+    }
+
+    [Test]
+    public void Should_infer_razor_language_for_razor_file()
+    {
+        // arrange
+        using var ctx = new BunitContext();
+        ctx.JSInterop.SetupVoid("Spillgebees.DocsSdk.copyToClipboard", _ => true);
+        ctx.JSInterop.SetupVoid("Spillgebees.DocsSdk.highlightElement", _ => true);
+        var files = new Dictionary<string, string> { ["Counter.razor"] = "<h1>Hello</h1>" };
+
+        // act
+        var cut = ctx.Render<CodeBlock>(parameters => parameters
+            .Add(p => p.Files, files)
+            .Add(p => p.Language, "razor"));
+
+        // assert
+        var code = cut.Find("pre code");
+        code.ClassList.Should().Contain("language-cshtml-razor");
+    }
+
+    [Test]
+    public void Should_switch_language_when_changing_active_tab_between_razor_and_code_behind()
+    {
+        // arrange
+        using var ctx = new BunitContext();
+        ctx.JSInterop.SetupVoid("Spillgebees.DocsSdk.copyToClipboard", _ => true);
+        ctx.JSInterop.SetupVoid("Spillgebees.DocsSdk.highlightElement", _ => true);
+        var files = new Dictionary<string, string>
+        {
+            ["Counter.razor"] = "<h1>Hello</h1>",
+            ["Counter.razor.cs"] = "public partial class Counter {}"
+        };
+
+        // act
+        var cut = ctx.Render<CodeBlock>(parameters => parameters
+            .Add(p => p.Files, files)
+            .Add(p => p.Language, "razor"));
+
+        // assert - first tab is razor
+        var code = cut.Find("pre code");
+        code.ClassList.Should().Contain("language-cshtml-razor");
+
+        // act - click the code-behind tab
+        var tabs = cut.FindAll(".code-block-tab");
+        tabs[1].Click();
+
+        // assert - should now be csharp
+        code = cut.Find("pre code");
+        code.ClassList.Should().Contain("language-csharp");
+    }
+
+    [Test]
+    public void Should_infer_language_from_cs_extension()
+    {
+        // arrange
+        using var ctx = new BunitContext();
+        ctx.JSInterop.SetupVoid("Spillgebees.DocsSdk.copyToClipboard", _ => true);
+        ctx.JSInterop.SetupVoid("Spillgebees.DocsSdk.highlightElement", _ => true);
+        var files = new Dictionary<string, string> { ["Program.cs"] = "var x = 1;" };
+
+        // act
+        var cut = ctx.Render<CodeBlock>(parameters => parameters.Add(p => p.Files, files));
+
+        // assert
+        var code = cut.Find("pre code");
+        code.ClassList.Should().Contain("language-csharp");
+    }
+
+    [Test]
+    public void Should_fall_back_to_language_parameter_for_unknown_extension()
+    {
+        // arrange
+        using var ctx = new BunitContext();
+        ctx.JSInterop.SetupVoid("Spillgebees.DocsSdk.copyToClipboard", _ => true);
+        ctx.JSInterop.SetupVoid("Spillgebees.DocsSdk.highlightElement", _ => true);
+        var files = new Dictionary<string, string> { ["script.py"] = "print('hello')" };
+
+        // act
+        var cut = ctx.Render<CodeBlock>(parameters => parameters
+            .Add(p => p.Files, files)
+            .Add(p => p.Language, "python"));
+
+        // assert
+        var code = cut.Find("pre code");
+        code.ClassList.Should().Contain("language-python");
+    }
 }
